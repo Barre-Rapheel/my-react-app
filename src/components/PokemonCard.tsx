36d1d8eea4a2bda2adde7e4a6dd614ec77340a32
Raphaël <barre.raphael97@gmail.com>
2024-10-22 15:12:28 +0200

    pokemonCard

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,9 @@
+function PokemonCard() {
+    return (
+      <figure>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" alt="Pikachu" />
+        <figcaption>"Pikachu"</figcaption>
+      </figure>
+    );
+  }
+export default PokemonCard;  
