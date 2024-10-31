529282353f370619ef770a0185df26d75fbbb0f2
Raphaël <barre.raphael97@gmail.com>
2024-10-31 11:45:07 +0100

    React8

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,23 +1,30 @@
+import './PokemonCard.css';
 
-
-  // Définition de l'interface Pokemon
 interface Pokemon {
-  name: string;       // name est obligatoire
-  imgSrc?: string;    // imgSrc est optionnel
+  name: string;
+  imgSrc?: string;
 }
 
-// Typage des props dans le composant PokemonCard
 function PokemonCard({ pokemon }: { pokemon: Pokemon }) {
+  const cardClass = `card ${
+    pokemon.name === 'bulbasaur' ? 'card-bulbasaur' :
+    pokemon.name === 'charmander' ? 'card-charmander' :
+    pokemon.name === 'squirtle' ? 'card-squirtle' :
+    pokemon.name === 'pikachu' ? 'card-pikachu' : 'card-no-image'
+  }`;
+
   return (
-    <figure>
+    <figure className={cardClass}>
       {pokemon.imgSrc ? (
-        <img src={pokemon.imgSrc} alt={pokemon.name} />
+        <img src={pokemon.imgSrc} alt={pokemon.name} className="cardImg" />
       ) : (
-        <p>???</p> // Si l'image n'existe pas, afficher "???"
+        <p className="no-image-text">???</p> // Affiche "???" en noir
       )}
-      <figcaption>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</figcaption>
+      <figcaption className="no-image-text">{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</figcaption>
     </figure>
   );
 }
 
-export default PokemonCard;  
+export default PokemonCard;
+
+
